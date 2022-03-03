50f6f92d36046368ca4e24c429803f98c707c107
Michael Scott <84039563+thebluenowhere@users.noreply.github.com>
2022-03-03 07:27:52 +0000

    Update Day8_Caesar_Cypher.py
    fixed a bug

--- Day8_Caesar_Cypher.py
+++ Day8_Caesar_Cypher.py
@@ -9,25 +9,13 @@ def encrypt(text, shift):
   cipher_text = ""
   for letter in text:
     new_index.append(alphabet.index(letter) + shift)
+  print(new_index)
   for index in new_index:
-    cipher_text += alphabet[index]
+    if index >= 26:
+      cipher_text += alphabet[index - 26]
+    else: 
+      cipher_text += alphabet[index]
   print(cipher_text)
   
   
 encrypt(text, shift)
-# >>> ["foo", "bar", "baz"].index("bar")
-# 1
-
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-    #e.g. 
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
-
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
-
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
-
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
