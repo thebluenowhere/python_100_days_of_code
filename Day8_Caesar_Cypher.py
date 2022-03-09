ee92cd873f1fce201fd6e9b74a110d7abd69d49b
Michael Scott <84039563+thebluenowhere@users.noreply.github.com>
2022-03-09 20:38:32 +0000

    Update Day8_Caesar_Cypher.py
    Finished challenge and tidied up the code. Fixed a bug.

--- Day8_Caesar_Cypher.py
+++ Day8_Caesar_Cypher.py
@@ -1,21 +1,31 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  
+  if cipher_direction == "decode":
+      shift_amount *= -1 
+  for char in start_text:
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]        
+    else:
+      end_text += char
 
-def encrypt(text, shift):
-  new_index = []
-  cipher_text = ""
-  for letter in text:
-    new_index.append(alphabet.index(letter) + shift)
-  print(new_index)
-  for index in new_index:
-    if index >= 26:
-      cipher_text += alphabet[index - 26]
-    else: 
-      cipher_text += alphabet[index]
-  print(cipher_text)
-  
-  
-encrypt(text, shift)
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+    
+from art import logo
+print(logo)
+
+restart = True
+while restart == True:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  #Make the index shift work even if number entered > 26 (i.e. len(list))
+  shift = shift % 26
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+  choice = input("Do you want to restart the program? Answer: Yes/No\n")
+  if choice != "yes".lower().strip():
+    restart = False
